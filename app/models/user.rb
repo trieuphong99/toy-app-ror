d9f5f0b70023b6208376a6d908a7862899182cb1
Trieu Phong <trieuphong328@gmail.com>
2019-12-08 13:46:38 +0700

    add hello

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	has_many :microposts
-	validates FILL_IN, presence: true
-	validates FILL_IN, presence: true 
+	validates :Name, presence: true
+	validates :Email, presence: true 
 end
